c2c461e42c72369eb6731b03feab169d2a271b9c
Erik Froese <froesecom@gmail.com>
2015-10-20 21:00:33 +1100

    add after initialize method

--- AsyncCsvImportApp/app/models/dirt_bag.rb
+++ AsyncCsvImportApp/app/models/dirt_bag.rb
@@ -1,21 +1,28 @@
 class DirtBag < ActiveRecord::Base
   include CsvImporter
 
-  def self.import_dirtbags_async
-    
+  attr_accessor :successes, :failures
+
+  after_initialize :init 
+
+  def import_dirtbags_async
     pool = CsvImporter::ImportActor.pool(size: 5)
     SmarterCSV.process("public/all_the_things.csv").map do |row|
       pool.future(:process_row, row)
-    end.map(&:value)
-    ActiveRecord::Base.clear_active_connections!
-    
+    end.map(&:value) 
   end
   
-  def self.import_dirtbags_insync
+  def import_dirtbags_insync
     pool = CsvImporter::ImportActor.pool(size: 5)
     SmarterCSV.process("public/all_the_things.csv") do |row|
       pool.process_row(row)
     end
+  end
+
+  private
+  def init 
+    self.successes = 0
+    self.failures  = []
   end
 
 end
